332539f28446d66d1bcfdeb3417bcc8bc7da3e3a
a-elef93 <a.eleftheriadis93@gmail.com>
2026-08-14 11:28:25 +0300

    Added the code for the hover color- effect on every square of the grid.

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const container = document.querySelector("#container");
+
+
 for(let i=0; i<256; i++){
     const grid = document.createElement("div");
     container.appendChild(grid);
+    grid.addEventListener("mouseover",() => {
+        grid.style.backgroundColor = "#497285";
+        setTimeout(() => {
+            grid.style.backgroundColor="";
+        }, 500);
+    });
 }
+
