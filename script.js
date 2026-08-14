7e3237494aae56c09e02c91c76afc9f69f715d5f
a-elef93 <a.eleftheriadis93@gmail.com>
2026-08-14 12:31:47 +0300

    Added two new functions, one for drawing the grid due to the user input and another that resets the grid every times the user re-inserts a new input.

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
 const container = document.querySelector("#container");
+const btn = document.querySelector("#regrid");
+const containerWidth = 960;
+const containerHeight = 960;
 
-for(let i=0; i<256; i++){
-    const grid = document.createElement("div");
-    container.appendChild(grid);
-    grid.addEventListener("mouseover",() => {
-        grid.style.backgroundColor = "#53a8b6";
-        // reset the color after a short delay 
-        setTimeout(() => {      
-            grid.style.backgroundColor="";
-        }, 600);
-    });
+btn.addEventListener("click", () =>{
+    resetGrid();
+    let squaresPerSide = Number(prompt("Enter the number of the squares per side (max:100)"));
+    while(squaresPerSide>100 || squaresPerSide<=0){
+        squaresPerSide = Number(prompt("Invalid value! Enter the number of the squares per side (max:100)"));
+    }
+    drawGrid(squaresPerSide);
+});
+
+function drawGrid(squares){
+    for(let i=0; i<(squares**2); i++){
+        const grid = document.createElement("div");
+        grid.style.width = containerWidth / squares + "px";
+        grid.style.height = containerHeight /squares + "px";
+        container.appendChild(grid);
+        grid.addEventListener("mouseover",() => {
+            grid.style.backgroundColor = "#53a8b6";
+            // reset the color after a short delay 
+            setTimeout(() => {      
+                grid.style.backgroundColor="";
+            }, 600);
+        });
+    }
 }
 
-const btn = document.querySelector("#regrid");
-btn.addEventListener("click", () =>{
-    let x = prompt("Enter the number of the squares per side (max:100)");
-    while(x>100 || x<=0){
-        x = prompt("Invalid value! Enter the number of the squares per side (max:100)");
-    }
-
-});
+function resetGrid(){
+    container.innerHTML = "";
+}
