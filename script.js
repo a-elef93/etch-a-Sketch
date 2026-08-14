7840ff5e337882065b8f2608e2b65d5778c2f260
a-elef93 <a.eleftheriadis93@gmail.com>
2026-08-14 12:01:15 +0300

    Added the btn event and  rule for the user input of grid sides

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const container = document.querySelector("#container");
 
-
 for(let i=0; i<256; i++){
     const grid = document.createElement("div");
     container.appendChild(grid);
     grid.addEventListener("mouseover",() => {
-        grid.style.backgroundColor = "#497285";
+        grid.style.backgroundColor = "#53a8b6";
         // reset the color after a short delay 
         setTimeout(() => {      
             grid.style.backgroundColor="";
@@ -13,3 +12,11 @@ for(let i=0; i<256; i++){
     });
 }
 
+const btn = document.querySelector("#regrid");
+btn.addEventListener("click", () =>{
+    let x = prompt("Enter the number of the squares per side (max:100)");
+    while(x>100 || x<=0){
+        x = prompt("Invalid value! Enter the number of the squares per side (max:100)");
+    }
+
+});
